5a61165ea12c90320e099867783735348d4860fa
Ryan Rodwell <ryanrodwell@gmail.com>
2018-02-21 16:36:43 -0500

    removing required format

--- AppointmentScheduler/scheduler/models.py
+++ AppointmentScheduler/scheduler/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Appointment(models.Model):
-    date = models.CharField(max_lenth=260)
+    date = models.CharField(max_length=260)
     time = models.CharField(max_length=10)
     description = models.CharField(max_length=260)
